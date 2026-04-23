8db935ab199d85d641d387f644b785e6965baad1
youzhen0827 <144466920+youzhen0827@users.noreply.github.com>
2026-04-24 01:04:39 +0800

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:3000/api'
+const API_BASE_URL = 'https://invoice-system-api-6fgc.onrender.com/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
